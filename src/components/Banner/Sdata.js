2ddab7cf85583047830ccda0e9e9edd09750d6d9
John-Abu-Al-Yamin <je0409389@gmaill.com>
2024-04-20 12:18:48 +0200

    Upload

---
+++ src/components/Banner/Sdata.js
@@ -0,0 +1,46 @@
+import img1 from "../UI/images/apple.png";
+import img2 from "../UI/images/banner-1.jfif";
+
+const Sdata = [
+  {
+    id: 1,
+    img: img1,
+    title: "iPhone 14 Series",
+    desc: "Up to 10%  ",
+    desc2: " off Voucher",
+    cover: img2,
+  },
+  {
+    id: 2,
+    img: img1,
+    title: "iPhone 14 Series",
+    desc: "Up to 10%  ",
+    desc2: " off Voucher",
+    cover: img2,
+  },
+  {
+    id: 3,
+    img: img1,
+    title: "iPhone 14 Series",
+    desc: "Up to 10%  ",
+    desc2: " off Voucher",
+    cover: img2,
+  },
+  {
+    id: 4,
+    img: img1,
+    title: "iPhone 14 Series",
+    desc: "Up to 10% ",
+    desc2: " off Voucher",
+    cover: img2,
+  },
+  {
+    id: 5,
+    img: img1,
+    title: "iPhone 14 Series",
+    desc: "Up to 10% ",
+    desc2: " off Voucher",
+    cover: img2,
+  },
+];
+export default Sdata;
